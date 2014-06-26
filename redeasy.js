38cec3a3a652edf38d135c7665d5a964e74483b7
pminervini <p.minervini@gmail.com>
2014-06-26 16:14:17 +0200

    more methods

--- redeasy.js
+++ redeasy.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var redis = require("redis");
+var redis = require('redis');
 
 var argv = require('yargs').argv;
 
@@ -44,17 +44,125 @@ app.get('/redis/set/:key/:value', function (req, res) {
 	});
 });
 
+app.get('/redis/del/:key', function (req, res) {
+	var key = req.params.key;
+	client.del(key, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/incr/:key', function (req, res) {
+	var key = req.params.key;
+	client.incr(key, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/decr/:key', function (req, res) {
+	var key = req.params.key;
+	client.decr(key, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+/* Expiration */push
+
+app.get('/redis/expire/:key/:value', function (req, res) {
+	var key = req.params.key, value = req.params.value;
+	client.expire(key, value, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/ttl/:key', function (req, res) {
+	var key = req.params.key;
+	client.ttl(key, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+/* Lists */
+
+app.get('/redis/lpop/:list', function (req, res) {
+	var list = req.params.list;
+	client.lpop(list, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/lpush/:list/:value', function (req, res) {
+	var list = req.params.list, value = req.params.value;
+	client.lpush(list, value, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/rpop/:list', function (req, res) {
+	var list = req.params.list;
+	client.rpop(list, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/rpush/:list/:value', function (req, res) {
+	var list = req.params.list, value = req.params.value;
+	client.rpush(list, value, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/lrange/:list/:lower/:upper', function (req, res) {
+	var list = req.params.list, lower = req.params.lower, upper = req.params.upper;
+	client.lrange(list, lower, upper, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/llen/:list', function (req, res) {
+	var list = req.params.list;
+	client.llen(list, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+/* Sets */
+
+app.get('/redis/sadd/:set/:value', function (req, res) {
+	var set = req.params.set, value = req.params.value;
+	client.sadd(set, value, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/srem/:set/:value', function (req, res) {
+	var set = req.params.set, value = req.params.value;
+	client.srem(set, value, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/sismember/:set/:value', function (req, res) {
+	var set = req.params.set, value = req.params.value;
+	client.sismember(set, value, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+app.get('/redis/smembers/:set', function (req, res) {
+	var set = req.params.set;
+	client.smembers(set, function (err, reply) {
+		http_redis(err, reply, res);
+	});
+});
+
+
+/* Server */
+
 app.get('/redis/version', function (req, res) {
 	http_redis(null, client.server_info.versions, res);
 });
 
 /* Utility methods for monitoring the service */
-
-var spawn = require('child_process').spawn;
-
-app.get('/uptime', function(req, res) {
-	spawn('uptime').stdout.pipe(res);
-});
 
 app.get('/health', function(req, res) {
 	server.getConnections(function (err, connections) {
